81f714ed902011c6a418d3d82b4af65c6899ad4c
venky <venkyiosdeveloper@gmail.com>
2024-04-01 17:56:41 -0400

    upload excel to server and modified logic is done in UI

---
+++ src/app/file-upload.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FileUploadService {
+
+  constructor(private http: HttpClient) { }
+
+  uploadFile(file: File): Observable<any> {
+    const formData: FormData = new FormData();
+    formData.append('file', file, file.name);
+    // Adjust the URL according to your API endpoint
+    return this.http.post<any>('http://localhost:3000/upload', formData);
+  }
+}
